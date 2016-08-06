f8f866a485f884b80a9dcd02d1cbed7a241e5a2e
ahardouin <alexandre.hardouin@laposte.net>
2016-08-06 16:15:20 +0200

    distribution graine avec gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,13 @@ var concat = require("gulp-concat");
 var rev = require('gulp-rev');
 var useref = require('gulp-useref');
 var clean = require('gulp-clean');
+var gulpif = require('gulp-if');
+var csso = require('gulp-csso');
+var minify = require('gulp-minify');
+var uglify = require('gulp-uglify');
+var filter = require('gulp-filter');
+var revReplace = require('gulp-rev-replace');
+var debug = require('gulp-debug');
 
 var dist="./dist";
 var dist_app = "./dist/app";
@@ -16,34 +23,61 @@ gulp.task('default', function() {
 });
 
 // distribution de l'application pour mise en production
-gulp.task('dist', ['clean', 'copyRessources'], function() {
-	return gulp.src(dist, {read: false})
-    .pipe(clean());
+gulp.task('dist', ['clean', 'copyRessources', 'build'], function() {
+	
 });
 
 // suppression du répertoire dist
 gulp.task('clean', function() {
-	
+	return gulp.src(dist, {read: false})
+    .pipe(clean());
 });
 
 //copie des ressources html dans le repertoire dist
-gulp.task('copyRessources', function() {
+gulp.task('copyRessources', ['clean'], function() {
 	gulp.src(['./app/**/*.html']).pipe(gulp.dest(dist_app));
 	gulp.src(['./app/img/**/*.*']).pipe(gulp.dest(dist_app + img));
 });
 
-//concatene, sourcemap les fichiers javascripts
+// concatene JS et CSS, minify CSS uglify JS et ajoute version
 gulp.task('build', ['clean'], function() {
+	
+	var jsFilter = filter("**/*.js");
+	var cssFilter = filter("**/*.css");
+	 
 	return gulp.src('./index.html')
 	.pipe(useref())
-    .pipe(gulp.dest(dist));
+	.pipe(sourcemaps.init())
+	.pipe(debug({title: 'init sourcemap'}))
+	.pipe(gulpif('*.css', csso()))
+	.pipe(gulpif('*.js', uglify({ mangle: false, compress:true, output: { beautify: false }})))
+	.pipe(gulpif(['*.js', '*.css'],rev())) 
+	.pipe(revReplace())   
+	.pipe(gulpif(['*.js', '*.css'],sourcemaps.write('.')))
+	.pipe(debug({title: 'write sourcemap'}))
+    .pipe(gulp.dest(dist))
+    ;
 });
 
-gulp.task('connect', ['index','watch'], function() {
+gulp.task('connect', ['index'], function() {
   connect.server({
     livereload: true
   });
 });
+
+gulp.task('connect:watch', ['index','watch'], function() {
+  connect.server({
+    livereload: true
+  });
+});
+
+gulp.task('connect:dist', function() {
+	  connect.server({
+	    livereload: false,
+	    root: dist,
+	    port: 7777
+	  });
+	});
 
 gulp.task('index', function () {
 	  var target = gulp.src('./index.html');
@@ -73,6 +107,3 @@ gulp.task('css', function () {
 	  gulp.src('./app/style/*.css')
 	    .pipe(connect.reload());
 	});
-
-	
-// modification avant de changer de branch.
