c3258baf2228fa8e583a69eb4a8c0f5df33533ac
ahardouin <alexandre.hardouin@laposte.net>
2016-08-08 09:16:04 +0200

    page home contient le descriptif du contenu du site

--- app/home/homeCtrl.js
+++ app/home/homeCtrl.js
@@ -6,21 +6,7 @@
 
 	function HomeCtrl($scope, $controller) { 
 		var vm = this;
-		//var rubrique2Ctrl = $controller("Rubrique2Ctrl",{$scope: $scope});
-		vm.models = {
-	        selected: null,
-	        lists: {"A": [{label: "Rubrique 1", include:"app/home/rubrique1.html"},
-	                      {label: "Rubrique 2", include:"app/home/rubrique4.html"}
-	                      ], 
-	                "B": [{label: "Rubrique 3", include:"app/home/rubrique3.html"},
-	                      {label: "Rubrique 4", include:"app/home/rubrique2.html"}]}
-	    };
-
-	    // Model to JSON for demo purpose
-	    $scope.$watch('vm.models', function(model) {
-	    	//rubrique2Ctrl.createGraphique();	
-	    	vm.modelAsJson = angular.toJson(model, true);
-	    }, true);
+		
 	}
 	
 })();
