ac194dc4b721656b80d23d6e02fa131f37d3f3f8
ahardouin <alexandre.hardouin@laposte.net>
2016-08-08 20:52:19 +0200

    ajout contenu gulpfile et jsdoc

--- app/home/homeCtrl.js
+++ app/home/homeCtrl.js
@@ -6,7 +6,10 @@
 
 	function HomeCtrl($scope, $controller) { 
 		var vm = this;
-		
+		vm.titreTachesGulp = "Tâches GULP du projet"
+		vm.texteTachesGulp = "Différentes tâches pour servir le projet, reloader les fichiers, distribuer le projet."	
+		vm.titreJscdoc3 = "Génération de la JSDOC";
+		vm.jsdoc3="La tâche gulp docGulpFile permet la génération de la documentation ";
 	}
 	
 })();
