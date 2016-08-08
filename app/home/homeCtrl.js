cf8d0eca7a499f9ac265e917c3547f98d957e10c
ahardouin <alexandre.hardouin@laposte.net>
2016-08-08 21:03:24 +0200

    contenu drag and drop avec composants integré avec nginclude

--- app/home/homeCtrl.js
+++ app/home/homeCtrl.js
@@ -6,10 +6,19 @@
 
 	function HomeCtrl($scope, $controller) { 
 		var vm = this;
+		
+		// labels
 		vm.titreTachesGulp = "Tâches GULP du projet"
 		vm.texteTachesGulp = "Différentes tâches pour servir le projet, reloader les fichiers, distribuer le projet."	
 		vm.titreJscdoc3 = "Génération de la JSDOC";
 		vm.jsdoc3="La tâche gulp docGulpFile permet la génération de la documentation ";
+		vm.titreDndNgInclude = "Drag and Drop avec angular-drag-and-drop-lists";
+		vm.texteDndNgInclude = "Test de Drag and Drop avec librairie angular-drag-and-drop-lists. Les composants à déplacer sont intégrés avec NgInclude";
+		vm.dndNgIncludeLabel = "DND avec ng-include"; // lien vers page dnd ng include
+		
+		// champs
+		vm.dndNgInclude = "dndNgInclude"; // state
+		
 	}
 	
 })();
