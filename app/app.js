2dd82d566777be6f1a0345d07ca12da6f296c65b
ahardouin <alexandre.hardouin@laposte.net>
2016-08-08 09:18:55 +0200

    ajout des routes

--- app/app.js
+++ app/app.js
@@ -3,15 +3,29 @@
 	angular.module("magraine", ['ui.router','ui.bootstrap','dndLists']);
 		
 	angular.module("magraine").config(
-			function($stateProvider, $urlRouterProvider, $locationProvider) {
+			function($stateProvider, $urlRouterProvider) {
 				
-				$locationProvider.html5Mode(true);
-
-				$urlRouterProvider.otherwise("/home");
+				$urlRouterProvider
+					.when("","/home")
+					.when("dndViewsUIRouter","/dndViewsUIRouter")
+					.otherwise("/home");
 
 				$stateProvider.state("home", {
 					url : "/home",
-					templateUrl : "app/home/home.html"
+					templateUrl : "/app/home/home.html"
+					
+				});
+				
+				$stateProvider.state("dndViewsUIRouter", {
+					url : "/dndViewsUIRouter",
+					templateUrl : "/app/dndViewsUIRouter/dndViewsUIRouter.html"
+					
+				});
+				
+				$stateProvider.state("dndNgInclude", {
+					url : "/dndNgInclude",
+					templateUrl : "/app/dndNgInclude/dndNgInclude.html"
+					
 				});
 			});
 	
