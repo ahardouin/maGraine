2f8c39c6fbe025a06315d604497ccc8ce75bb909
ahardouin <alexandre.hardouin@laposte.net>
2016-08-09 15:55:58 +0200

    ajout du contenu dnd avec vues angular ui router

--- app/home/homeCtrl.js
+++ app/home/homeCtrl.js
@@ -15,9 +15,13 @@
 		vm.titreDndNgInclude = "Drag and Drop avec angular-drag-and-drop-lists";
 		vm.texteDndNgInclude = "Test de Drag and Drop avec librairie angular-drag-and-drop-lists. Les composants à déplacer sont intégrés avec NgInclude";
 		vm.dndNgIncludeLabel = "DND avec ng-include"; // lien vers page dnd ng include
+		vm.dndViewsUIRouterLabel = "DND avec multiples vues (UIRouter)";// lien vers page dnd views ui router
+		vm.titreDndViewsUIRouter = "Drag and Drop avec vues multiples Angular UI Router";
+		vm.texteDndViewsUIRouter = "Test de Drag and Drop avec librairie angular-drag-and-drop-lists. Les composants à déplacer sont intégrés avec des vues multiples angular UI Router";
 		
 		// champs
 		vm.dndNgInclude = "dndNgInclude"; // state
+		vm.dndViewsUIRouter = "dndViewsUIRouter.rubriques"; // state
 		
 	}
 	
