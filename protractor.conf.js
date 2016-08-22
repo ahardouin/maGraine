70a8678c835b8ba5d1040cc4677e4414421628e1
ahardouin <alexandre.hardouin@laposte.net>
2016-08-22 09:39:19 +0200

    configuration protractor

---
+++ protractor.conf.js
@@ -0,0 +1,10 @@
+exports.config = {
+  framework: 'jasmine',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  capabilities: {
+    browserName: 'chrome'
+  },
+  jasmineNodeOpts: {
+    showColors: true, // Use colors in the command line report.
+  }
+};
